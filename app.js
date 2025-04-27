09221fe85f464ea8f8cdfa3e9d83fd656f99d54a
Hassan12271 <hassandanyal18@gmail.com>
2025-04-27 19:05:32 +0500

    add to cart function update

--- app.js
+++ app.js
@@ -1,14 +1,75 @@
 let itemCount = 0;
 let totalPrice = 0;
+let cart = [];
+
+let sidebarmenu = document.querySelectorAll(".side-bar-menu")[0];
+let items;
 
 function addToCart(price) {
+  let currentProductPrice = Number(price.parentNode.querySelector("p").innerHTML.slice(1));
   itemCount++;
-  totalPrice += Number(price.parentNode.querySelector("p").innerHTML.slice(1));
+  totalPrice += currentProductPrice;
   document.getElementById('item-count').innerText = itemCount;
   document.getElementById('total-price').innerText = totalPrice;
+  let currentProductNAme = price.parentNode.childNodes[3].innerHTML;
+  let currentProductImg = price.parentNode.childNodes[1].src;
+  items = {
+    productImage: currentProductImg,
+    productName: currentProductNAme,
+    productPRice: currentProductPrice,
+    productQuantity: 1
+  }
+  fillArry()
+  console.log(cart);
+  sidebarmenu.style.right = "0";
+  sidebarmenu.style.width = "100%";
+}
+
+function closebtn() {
+  sidebarmenu.style.right = "-100%";
+  sidebarmenu.style.width = "0";
+}
+
+function fillArry() {
+  let found = false;
+
+  for (let i = 0; i < cart.length; i++) {
+    if (
+      cart[i].productImage === items.productImage &&
+      cart[i].productName === items.productName &&
+      cart[i].productPRice === items.productPRice
+    ) {
+      found = true;
+      cart[i].productQuantity += 1;
+      cart[i].productPRice += cart[i].productPRice;
+      break;
+    }
+  }
+
+  if (found) {
+    createInSideMenu();
+    
+  } else {
+    cart.push(items);
+    createInSideMenu();
+  }
 }
 
 
-for(let i = 0; i < 3; i++){
-    setTimeout(() => console.log(i),0)
+function createInSideMenu() {
+  let sectionEl = sidebarmenu.querySelector("section .cart-continer");
+  let htmlContent = '';
+
+  for (let i = 0; i < cart.length; i++) {
+    htmlContent += `<div class="inner-container">
+      <div class="col-1"><img src="${cart[i].productImage}" alt="Product Image"></div>
+      <div class="col-2">
+        <h3>Product Name: ${cart[i].productName}</h3>
+        <p>Product Price: ${cart[i].productPRice}$</p>
+        <p>Product Quantity: <span class="count-product">${cart[i].productQuantity}</span></p>
+      </div>
+    </div>`;
+  }
+
+  sectionEl.innerHTML = htmlContent;
 }
